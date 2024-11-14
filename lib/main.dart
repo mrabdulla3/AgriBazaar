5a2a1be884fa9cdf76bd3419015a8cb4cfcfff83
Mohd Abdulla <abdullagaur6559@gmail.com>
2024-11-14 23:37:21 +0530

    Add .env file to .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:agribazar/splashScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await dotenv.load();
+  } catch (e) {
+    print("Error loading .env file: $e");
+  }
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
