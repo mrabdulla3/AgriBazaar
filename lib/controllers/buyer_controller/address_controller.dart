edbc4eea9bf7beda8aba0bc92b10e3ee9eb68876
Abdulla Gaur <abdullagaur6559@gmail.com>
2025-03-13 01:29:56 +0530

    Search Product and Sidebar state management done

--- lib/controllers/buyer_controller/address_controller.dart
+++ lib/controllers/buyer_controller/address_controller.dart
@@ -1,3 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
-class AddressController extends GetxController {}
+class AddressController extends GetxController {
+  AddressController({required this.user});
+  User? user;
+  final formKey = GlobalKey<FormState>();
+  final addressController = TextEditingController();
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadUserAddress(); // Load the current address from Firestore
+  }
+
+  Future<void> loadUserAddress() async {
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user!.uid)
+        .get();
+
+    if (userDoc.exists) {
+      addressController.text = userDoc.get('address') ?? '';
+    }
+  }
+
+  Future<void> saveAddress() async {}
+
+  @override
+  void onClose() {
+    super.onClose();
+    addressController.dispose();
+  }
+}
